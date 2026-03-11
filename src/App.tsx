e5c685389a51f5b3ef580eb48fbd7028eb3da801
Ruslan Deyarov <r.deyarov@applicaster.com>
2026-03-12 01:05:01 +0400

    drop unused Badge

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
-import Badge from "react-bootstrap/Badge";
 import InputGroup from "react-bootstrap/InputGroup";
 import "bootstrap/dist/css/bootstrap.min.css";
 import {
