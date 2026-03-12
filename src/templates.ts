14aa0ca0e1abc3624e58e038e75e2eb1e32371be
Ruslan Deyarov <r.deyarov@applicaster.com>
2026-03-12 14:32:38 +0400

    get rid of branch for tvos for v15

--- src/templates.ts
+++ src/templates.ts
@@ -91,7 +91,6 @@ export const platformTemplates: PlatformTemplates = {
           commands: [
             "cd ${HOME}/projects/applicaster/tvos/QuickBrick",
             "git reset --hard HEAD && git clean -xfd",
-            "git checkout main",
             "yarn && yarn add react-native@npm:react-native-tvos@0.77.2-1 && yarn zapplicaster prepare -a -b <APP_ID>",
             '/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/sm00th1980/applicaster-dev/HEAD/tvos/v15/setup.sh)"',
           ],
