3149df6de84711d236c73b6559c2fc79a4bc3b87
Ruslan Deyarov <r.deyarov@applicaster.com>
2026-04-13 13:35:37 +0400

    fix path

--- src/templates/v15.ts
+++ src/templates/v15.ts
@@ -34,7 +34,7 @@ export const v15 = [
           "cd ${HOME}/projects/applicaster/android/QuickBrick",
           "git reset --hard HEAD && git clean -xfd",
           "yarn && yarn zapplicaster prepare -a <APP_ID>",
-          '/bin/bash -c "$(curl -fsSL https://applicaster-dev-setup.vercel.app/setup/android_tv/setup.sh)"',
+          '/bin/bash -c "$(curl -fsSL https://applicaster-dev-setup.vercel.app/setup/androidtv/setup.sh)"',
           "yarn start",
         ],
       },
