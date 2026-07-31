048bee30c2a35901b71e49c2915cb1bf972e967b
Ruslan Deyarov <r.deyarov@applicaster.com>
2026-07-31 16:05:45 +0400

    added patch

--- src/templates/v16.ts
+++ src/templates/v16.ts
@@ -128,6 +128,7 @@ export const v16 = [
           "cd ${HOME}/projects/applicaster/androidtv/QuickBrick",
           "rm -f ./android/main.hbc",
           "rm -f ../zapp-platform-android/app/build/outputs/apk/tvGoogle/release/app-tv-google-release.apk",
+          '/bin/bash -c "$(curl -fsSL https://applicaster-dev-setup.vercel.app/setup/androidtv/patch-package-json.sh)"',
           "yarn build:ts && yarn build:android && yarn build:hermes:android",
           "cp ./android/main.hbc ../zapp-platform-android/app/src/tvGoogle/assets",
           "cd ${HOME}/projects/applicaster/androidtv/zapp-platform-android",
