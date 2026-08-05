9f93d57c40d97261c13bd4f148eec9280d2e7545
Ruslan Deyarov <r.deyarov@applicaster.com>
2026-08-05 18:55:24 +0400

    fix deploy to emulator

--- src/templates/v15.ts
+++ src/templates/v15.ts
@@ -51,7 +51,7 @@ export const v15 = [
         name: "Deploy to emulator",
         commands: [
           "cd ${HOME}/projects/applicaster/android/zapp-platform-android",
-          "adb reverse tcp:8081 tcp:8081 && ./gradlew installMobileGoogleDebug -PREACT_NATIVE_PACKAGER_ROOT=localhost:8081",
+          "adb -s emulator-5554 reverse tcp:8081 tcp:8081 && ANDROID_SERIAL=emulator-5554 ./gradlew installMobileGoogleDebug -PREACT_NATIVE_PACKAGER_ROOT=localhost:8081",
         ],
       },
       {
